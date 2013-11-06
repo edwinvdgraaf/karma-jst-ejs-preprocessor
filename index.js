5a7acf4b8a065c8954d42324718bbacbdc8c2894
Edwin van der Graaf <dev@edwingraaf.nl>
2013-11-06 19:57:25 +0100

    Real basic support for mountPath

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ var createJstEjsPreprocessor = function(args, config, logger, helper) {
   // Removes base and sprockets untill sprockets mountPath with options.logicalPathStrip
   var transformLogicalPath = function(filepath) {
     var logicalPath = filepath.replace(/\.jst.ejs$/, '');
-    
-    return logicalPath;
+    // Poor mans solutions here, it somewhat more complex
+
+    return logicalPath.substring(logicalPath.indexOf(options.logicalMountPoint));
   };
 
   // Internal (private) namespace storage
